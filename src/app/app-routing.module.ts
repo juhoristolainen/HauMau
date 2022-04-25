89340c0a6a53be3cbb355c30ccef02e0e0aac320
Juho <n3649@student.jamk.fi>
2022-04-25 12:49:07 +0300

    contact page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { AboutComponent } from './about/about.component';
 import { AnimalsComponent } from './animals/animals.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'frontpage', pathMatch: 'full' },
   { path: 'frontpage', component: FrontpageComponent },
   { path: 'about', component: AboutComponent },
   { path: 'animals', component: AnimalsComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FrontpageComponent } from './frontpage/frontpage.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { AnimalsComponent } from './animals/animals.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AnimalsComponent } from './animals/animals.component';
     FrontpageComponent,
     FooterComponent,
     AboutComponent,
-    AnimalsComponent
+    AnimalsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
